9b23afe13c1302e81fa90b78871b92264e32b026
zhuojuns-cmu-F24 <zhuojuns@andrew.cmu.edu>
2024-11-16 00:58:11 -0800

    port

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   base: '/Portfolio/',
   server: {
-    port: 80, // 设置端口号
-    host: '0.0.0.0', // 可选：指定主机地址
+    port: 3000, 
+    host: '0.0.0.0', 
   },
 })
