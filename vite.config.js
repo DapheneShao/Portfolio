68958350b0238e7734987a5731d50e199f6e018a
zhuojuns-cmu-F24 <zhuojuns@andrew.cmu.edu>
2024-11-15 23:00:10 -0800

    : qa:

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/'
+  base: '/Portfolio/',
+  server: {
+    port: 80, // 设置端口号
+    host: '0.0.0.0', // 可选：指定主机地址
+  },
 })
